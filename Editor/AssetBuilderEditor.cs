99c77ee8e44a97ea463af49aa5f0aab9ae6624b6
cstria0106 <cstria0106@gmail.com>
2024-09-08 20:40:26 +0900

    fix: asset builder dependency add: clean up

--- Editor/AssetBuilderEditor.cs
+++ Editor/AssetBuilderEditor.cs
@@ -1,10 +1,8 @@
 using System.IO;
-using System.Runtime.InteropServices;
-using System.Runtime.Remoting;
+using System.IO.Compression;
 using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using SEECHAK.SDK.Editor.Core;
-using Unity.VisualScripting.IonicZip;
 using UnityEditor;
 using UnityEngine;
 
@@ -12,12 +10,6 @@ namespace SEECHAK.SDK.Editor
 {
     public class AssetBuilderEditor : EditorWindow
     {
-        [MenuItem("SEECHAK/Build Asset")]
-        public static void ShowWindow()
-        {
-            GetWindow<AssetBuilderEditor>("Build Asset");
-        }
-
         private void OnGUI()
         {
             var objects = Selection.gameObjects;
@@ -31,12 +23,9 @@ namespace SEECHAK.SDK.Editor
                 var style = new GUIStyle(EditorStyles.label)
                 {
                     fontSize = 10,
-                    margin = new RectOffset(0, 0, 0, 0),
+                    margin = new RectOffset(0, 0, 0, 0)
                 };
-                foreach (var obj in objects)
-                {
-                    EditorGUILayout.LabelField(obj.name, style);
-                }
+                foreach (var obj in objects) EditorGUILayout.LabelField(obj.name, style);
             }
 
             if (GUILayout.Button("Build & Save"))
@@ -53,29 +42,36 @@ namespace SEECHAK.SDK.Editor
             Repaint();
         }
 
+        [MenuItem("SEECHAK/Build Asset")]
+        public static void ShowWindow()
+        {
+            GetWindow<AssetBuilderEditor>("Build Asset");
+        }
+
         private async Task BuildAndSave(GameObject[] objects)
         {
             var path = EditorUtility.SaveFilePanel("Save Asset", Application.dataPath, "Assets", "zip");
-            if (string.IsNullOrEmpty(path))
-            {
-                return;
-            }
+            if (string.IsNullOrEmpty(path)) return;
 
             var tempFilePath = Temp.GetPath("Asset.zip");
-            using var archive = new ZipFile(tempFilePath);
-
-            foreach (var obj in objects)
+            if (File.Exists(tempFilePath)) AssetDatabase.DeleteAsset(tempFilePath);
+            using (var archive = ZipFile.Open(tempFilePath, ZipArchiveMode.Create))
             {
-                var result = AssetBuilder.Build(obj);
-                var assetBytes = await File.ReadAllBytesAsync(result.Path);
-                archive.AddEntry(obj.name, assetBytes);
+                foreach (var obj in objects)
+                {
+                    AssetBuilder.CleanUp();
+                    var result = AssetBuilder.Build(obj);
+                    var entry = archive.CreateEntry(obj.name);
+                    await using var zipStream = entry.Open();
+                    await using var fileStream = new FileStream(result.Path, FileMode.Open);
+                    await fileStream.CopyToAsync(zipStream);
+                }
             }
 
-            archive.Save();
+            AssetBuilder.CleanUp();
             File.Delete(path);
             File.Move(tempFilePath, path);
             EditorUtility.DisplayDialog("Success", "Asset has been saved", "OK");
         }
     }
-
 }
